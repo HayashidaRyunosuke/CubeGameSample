396ff46630e5d5f13800d1f75978df6ffba294dc
HayashidaRyunosuke <rinryuchan7808@gmail.com>
2020-12-10 16:16:01 +0900

    init

---
+++ Assets/__Scripts/Cube/AutoCubeGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using CubeGameSample;
+using UnityEngine;
+
+public class AutoCubeGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject cubePrefab;
+
+    [SerializeField]
+    private int maxCube = 1000;
+    
+    [SerializeField]
+    private int seed = 0;
+
+    private GameObject root;
+    
+    void Start()
+    {
+        Random.seed = seed;
+        AutoGenerate();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AutoGenerate();
+        }
+    }
+
+    public void AutoGenerate()
+    {
+        if (root) Destroy(root);
+        root = new GameObject();
+        root.transform.position = Vector3.zero;
+        CubeBehaviour prevCube = null;
+        for (int i = 0;i<maxCube;i++)
+        {
+            GameObject obj = Instantiate(cubePrefab);
+            obj.transform.parent = root.transform;
+            CubeBehaviour cube = obj.GetComponent<CubeBehaviour>();
+            if (prevCube == null)
+            {
+                prevCube = cube;
+                continue;
+            }
+
+            int rand = Random.Range(0,6);
+
+            switch (rand)
+            {
+                case 0:
+                    CubeBehaviour.SetRight(prevCube,cube);
+                    break;
+                case 1:
+                    CubeBehaviour.SetLeft(prevCube,cube);
+                    break;
+                case 2:
+                    CubeBehaviour.SetBack(prevCube,cube);
+                    break;
+                case 3:
+                    CubeBehaviour.SetFront(prevCube,cube);
+                    break;
+                case 4:
+                    CubeBehaviour.SetUpper(prevCube,cube);
+                    break;
+                case 5:
+                    CubeBehaviour.SetLower(prevCube,cube);
+                    break;
+            }
+
+            prevCube = cube;
+        }
+    }
+}
---
+++ Assets/__Scripts/Cube/CubeBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace CubeGameSample
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CubeBehaviour : MonoBehaviour
+    {
+        public bool IsTrigger
+        {
+            get { return true; }
+        }
+
+        private BoxCollider _boxCollider;
+        
+        public BoxCollider BoxCol
+        {
+            get
+            {
+                if (!_boxCollider) _boxCollider = GetComponent<BoxCollider>();
+                return _boxCollider;
+            }
+        }
+
+        public Vector3 UpperRightBackCornerPos
+        {
+            get
+            {
+                return new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y + transform.localScale.y/2,transform.position.z + transform.localScale.z/2);
+            }
+        }
+        public Vector3 LowerLeftFrontCornerPos
+        {
+            get
+            {
+                return new Vector3(transform.position.x + transform.localScale.x/2,transform.position.y - transform.localScale.y/2,transform.position.z - transform.localScale.z/2);
+            }
+        }
+
+        public static void SetRight(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.UpperRightBackCornerPos.x - root.gameObject.transform.localScale.x/2,root.UpperRightBackCornerPos.y - root.gameObject.transform.localScale.y/2,root.UpperRightBackCornerPos.z - root.gameObject.transform.localScale.z/2);   
+        }
+        
+        public static void SetLeft(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.LowerLeftFrontCornerPos.x + root.gameObject.transform.localScale.x/2,root.LowerLeftFrontCornerPos.y + root.gameObject.transform.localScale.y/2,root.LowerLeftFrontCornerPos.z + root.gameObject.transform.localScale.z/2);   
+        }
+        
+        public static void SetFront(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.gameObject.transform.position.x,root.gameObject.transform.position.y,root.UpperRightBackCornerPos.z + root.gameObject.transform.localScale.z/2);   
+        }
+        
+        public static void SetBack(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.gameObject.transform.position.x,root.gameObject.transform.position.y,root.LowerLeftFrontCornerPos.z - root.gameObject.transform.localScale.z/2);   
+        }
+        
+        public static void SetUpper(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.gameObject.transform.position.x,root.UpperRightBackCornerPos.y + root.gameObject.transform.localScale.y/2,root.gameObject.transform.position.z);   
+        }
+        
+        public static void SetLower(CubeBehaviour root,CubeBehaviour child)
+        {
+            child.gameObject.transform.position = new Vector3(root.gameObject.transform.position.x,root.LowerLeftFrontCornerPos.y - root.gameObject.transform.localScale.y/2,root.gameObject.transform.position.z);   
+        }
+        
+    }
+}
